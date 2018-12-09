06d4323f53b97f15a32c830c75f712be44e51c1c
JohnnySn0w <michael.mahan43@gmail.com>
2018-12-08 20:08:33 -0600

    remove .vagrant

--- Vagrantfile
+++ Vagrantfile
@@ -9,7 +9,7 @@ Vagrant.configure("2") do |config|
 
   # Every Vagrant development environment requires a box. You can search for
   # boxes at https://vagrantcloud.com/search.
-  config.vm.box = "ubuntu/xenial64"
+  config.vm.box = "ubuntu/bionic64"
 
   # Create a forwarded port mapping which allows access to a specific port
   # within the machine from a port on the host machine. In the example below,
--- envConfig.sh
+++ envConfig.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 set -e
 HOME=/home/vagrant
-echo "1) Logged in, ensuring correct landing"
+echo "Ensuring correct landing"
 if ! grep -q "cd /vagrant" $HOME/.bashrc ; then 
     echo "cd /vagrant" >> $HOME/.bashrc 
     echo "Corrected..."
 fi 
+if ! grep -q "alias dock='docker ps -a'" $HOME/.bashrc ; then 
+    echo "alias dock='docker ps -a'" >> $HOME/.bashrc
+fi 
 echo "Landing..."
 
-echo "2) Installing packages"
+echo "Installing packages"
 sudo apt-get -qq update \
     && apt-get -qq install \
         gcc \
@@ -16,7 +19,30 @@ sudo apt-get -qq update \
         npm \
         nodejs \
         ntp \
-        python-dev \
-        python-pip
-echo "3) Installing Updates"
+        apt-transport-https \
+        ca-certificates \
+        software-properties-common
+echo "Downloading and verifying Docker"
+sudo apt-key fingerprint 0EBFCD88
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get -qq update && sudo apt-get -qq install docker-ce
+echo "Pulling dynamo container"
+docker pull amazon/dynamodb-local
+echo "Setting user permissions"
+sudo usermod -aG docker vagrant
+echo "Updating Node"
+sudo npm i -g n
+sudo n latest
+echo "Installing Yarn"
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get -qq update && sudo apt-get -qq install yarn
+sudo chown -R $USER:$GROUP ~/.npm
+sudo chown -R $USER:$GROUP ~/.config
+echo "Installing Updates"
 sudo apt-get -qq update && apt-get -qq upgrade
+echo "Done"
