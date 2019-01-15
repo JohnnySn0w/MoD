8d7c1ef75d8d3fc9991a787fa148526359c82172
JohnnySn0w <michael.mahan43@gmail.com>
2019-01-14 23:13:22 -0600

    chunk of progress

---
+++ dbhandler.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const databaseManager = require('./databaseManager');
+const uuidv1 = require('uuid/v1');
+
+function createResponse(statusCode, message) {
+  return {
+    statusCode: statusCode,
+    body: JSON.stringify(message)
+  };
+}
+
+module.exports.saveItem = (event, context, callback) => {
+  const item = JSON.parse(event.body);
+  console.log(item);
+  item.itemId = uuidv1();
+
+  databaseManager.saveItem(item).then(response => {
+    console.log(response);
+    callback(null, createResponse(200, response));
+  });
+};
+
+module.exports.getItem = (event, context, callback) => {
+  const itemId = event.pathParameters.itemId;
+
+  databaseManager.getItem(itemId).then(response => {
+    console.log(response);
+    callback(null, createResponse(200, response));
+  });
+};
+
+module.exports.deleteItem = (event, context, callback) => {
+  const itemId = event.pathParameters.itemId;
+
+  databaseManager.deleteItem(itemId).then(response => {
+    callback(null, createResponse(200, 'Item was deleted'));
+  });
+};
+
+module.exports.updateItem = (event, context, callback) => {
+  const itemId = event.pathParameters.itemId;
+
+  const body = JSON.parse(event.body);
+  const paramName = body.paramName;
+  const paramValue = body.paramValue;
+
+  databaseManager.updateItem(itemId, paramName, paramValue).then(response => {
+    console.log(response);
+    callback(null, createResponse(200, response));
+  });
+};
+
+module.exports.triggerStream = (event, context, callback) => {
+  console.log('trigger stream was called');
+
+  const eventData = event.Records[0];
+  //console.log(eventData);
+
+  console.log(eventData.dynamodb.NewImage);
+  callback(null, null);
+};
