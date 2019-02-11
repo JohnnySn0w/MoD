a520c45844874b1c62114b587e7101d0dc5aed21
DJSanti <samantha.e.santiago@gmail.com>
2019-02-10 21:06:50 -0600

    Moved initialization of player stats to start, kept stats command to just telling you what your stats are

--- DiscordBot/commands/mud/start.js
+++ DiscordBot/commands/mud/start.js
@@ -1,4 +1,6 @@
 const commando = require('discord.js-commando');
+var players = require('../../player-stats'); // place to hold our stats for all players (will replace with database soon)
+const fs = require('fs');
 
 var entryRoomRole;
 
@@ -14,6 +16,28 @@ class StartCommand extends commando.Command {
 
     async run(message, args) {
         entryRoomRole = message.guild.roles.find(role => role.name === "entry-room");
+
+        // moved the initialization of player stats from stats to start - Santiago
+        var value = args.value; // lets us use our args above
+        var player = message.member; // gets player name
+        var play_id = message.member.id; // gets player id
+        var health = 100; // starting health value for all
+        var level = 1;
+        var strength = 7;
+        var defense = 5;
+        console.log(value);
+
+        for (var i = 0; i < players.length; i++)
+        {
+            // this places a new player into our players
+            if (play_id != players[i].play_id)
+            {
+                message.reply("you have no stats yet.");
+                players[i] = {player, play_id, health, level, strength, defense};
+                
+                message.reply("you now have stats");                  
+            }
+        }
 
         if (message.channel.name == 'test-zone') {
             if (message.member.roles.some(r=>["entry-room", "room-0", "room-1", "room-2"].includes(r.name)) ) {
--- DiscordBot/commands/mud/stats.js
+++ DiscordBot/commands/mud/stats.js
@@ -1,6 +1,5 @@
 const commando = require('discord.js-commando');
-const index = require('../../player-stats'); // place to hold our stats for all players (will replace with database soon)
-
+var players = require('../../player-stats');
 
 class StatsCommand extends commando.Command {
     constructor(client) {
@@ -8,75 +7,40 @@ class StatsCommand extends commando.Command {
             name: 'stats',
             group: 'mud',
             memberName: 'stats',
-            description: 'Shows player stats for the player who calls for them',
-            // args lets us type in a number to test that we can decrease our health
-            args: [
-                {
-                    key: 'value',
-                    prompt: 'how much damage to inflict?',
-                    type: 'integer',
-                    default: "0"
-                }
-            ]
-        
+            description: 'Shows player stats for the player who calls for them'      
         });
     }
 
     async run(message, args) {
+        var play_id = message.member.id; 
+        console.log(players.length);
 
-        var value = args.value; // lets us use our args above
-        var player = message.member; // gets player name
-        var play_id = message.member.id; // gets player id
-        var health = 100; // starting health value for all
-        var level = 1;
-        var strength = 7;
-        var defense = 5;
-        console.log(value);
-        var damage = Number(value); // makes our args a number we can use
-
-        // checks through our index of players to see what to do next
-        for (let i = 0; i < index.players.length; i++)
+        // checks through our players of players to see what to do next
+        for (var i = 0; i < players.length; i++)
         {
-            // this places a new player into our index
-            if (play_id != index.players[i].play_id)
+            // this places a new player into our players
+            if (play_id == players[i].play_id)
             {
-                message.reply("you have no stats yet.");
-                index.players[i] = {player, play_id, health, level, strength, defense};
-                message.reply("you now have stats");
-                message.reply("Health: " + index.players[i].health);
-                message.reply("Level: " + index.players[i].level);
-                message.reply("Strength: " + index.players[i].strength);
-                message.reply("Defense: " + index.players[i].defense);
-                
+                message.reply("Health: " + players[i].health);
+                message.reply("Level: " + players[i].level);
+                message.reply("Strength: " + players[i].strength);
+                message.reply("Defense: " + players[i].defense);                
             }
-            // player already exists in the index
-            else {
-                message.reply("you exist!");
-            }
-            // damage handling
-            if (damage != 0) {
-                message.reply("a hit!");
-                var dam = new Number(index.players[i].health) - damage;
-                index.players[i].health = dam;
-                message.reply(index.players[i].health);
-            }
-                else {
-                message.reply("no damage dealt");
-            }
+        
             // warning if your health is low
-            if (index.players[i].health > 0 && index.players[i].health < 11) {
+            if (players[i].health > 0 && players[i].health < 11) {
                 message.reply("You're actually about to die, my dude.");
             }
             // resets health if you "die"
-            if (index.players[i].health <= 0) {
-                index.players[i].health = 100;
+            if (players[i].health <= 0) {
+                players[i].health = 100;
                 message.reply("Dead. Resetting health. Now back at 100.");
             }
+        }
             
         }
         
     }
     
-}
 
 module.exports = StatsCommand;
