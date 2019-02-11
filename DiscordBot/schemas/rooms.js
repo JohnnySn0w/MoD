59b5e17156c9b8331c1ece37875c923c1411a6b0
Eric Ortiz <e.ortiz.programmer@gmail.com>
2019-02-10 20:45:20 -0600

    Forgot to add the rooms.js file to the last commit. Fixed it.

---
+++ DiscordBot/schemas/rooms.js
@@ -0,0 +1,113 @@
+module.exports = [
+    {
+        "name": "",
+        "id": "",
+        "description": "",
+        "exits": {
+            
+        },
+        "items": [
+
+        ],
+        "npcs": [
+
+        ],
+        "enemies": [
+
+        ]
+    },
+    {
+        "name": "entry-room",
+        "id": "530099209614000159",
+        "roleid": "530096793422266428",
+        "description": "You're in a large, cavernous room. Your footsteps echo into the darkness. Your journey begins to the north... \nThere's also an old-man and a little-boy in the room with you.",
+        "exits": {
+            "north": "room-0"
+        },
+        "items": [
+
+        ],
+        "npcs": [
+            {
+                "name": "old-man",
+                "formattedName": "Old Man",
+                "greeting": "I'm so old!!!!",
+                "responses": {
+                    "How are you?": "My back hurts because I am O L D !!!!",
+                    "Where are we?": "I don't know!! My back hurts so bad. I'm so old!"
+                },
+                "description": "He's bent over and seems to be upset about something..."
+            }, 
+            {
+                "name": "little-boy",
+                "formattedName": "Little Boy",
+                "greeting": "I'm a little boy!!!!!!",
+                "responses": {
+                    "How are you?": "I'm so great! I'm a little boy!",
+                    "Where are we?": "How would I know? I'm a little boy!"
+                },
+                "description": "It seems like he's a little boy."
+            }
+        ],
+        "enemies": [
+
+        ]
+    },
+    {
+        "name": "room-0",
+        "id": "530099456645791748",
+        "roleid": "530097563748139028",
+        "description": "The cave walls narrow in and create a hallway of sorts. The hallways comes to a split where the west path leads to light while the east path leads to a pile of rocks. To the south is a large room.",
+        "exits": {
+            "west": "room-1",
+            "south": "entry-room"
+        },
+        "items": {
+            "boulders": 2,
+            "rocks": 2,
+        },
+        "npcs": [
+
+        ],
+        "enemies": [
+
+        ]
+    },
+    {
+        "name": "room-1",
+        "id": "530099492091985921",
+        "roleid": "530097609428172801",
+        "description": "The roomly is oddly shaped like a perfect cube. The stone walls are completely smooth. On the far wall, there's ladder. To the east is a hallway.",
+        "exits": {
+            "up": "room-2",
+            "east": "room-0"
+        },
+        "items": {
+            "ladder": 1,
+        },
+        "npcs": [
+
+        ],
+        "enemies": [
+
+        ]
+    },
+    {
+        "name": "room-2",
+        "id": "530099522940960799",
+        "roleid": "530097635449634828",
+        "description": "Light floods your eyes as you arrive at the surface. There seems to only be flat grasses for miles. Only the ladder at your feet breaks up the landscape.",
+        "exits": {
+            "down": "room-1"
+        },
+        "items": {
+            "ladder": 0,
+        },
+        "npcs": [
+
+        ],
+        "enemies": [
+
+        ]
+    },
+]
