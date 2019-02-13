a46b80ad153919f5fdeade47ec01a4e8bc64d4db
DJSanti <samantha.e.santiago@gmail.com>
2019-02-12 21:36:27 -0600

    Figured out how to delete command messages sent by the player. You need to place message.delete() near the start of the run command so that it deletes before anything else happens. I also figured out how to DM players certain info. Finally, I changed some of the text to be more general when referring to players.

--- DiscordBot/commands/mud/stats.js
+++ DiscordBot/commands/mud/stats.js
@@ -15,26 +15,28 @@ class StatsCommand extends commando.Command {
         var play_id = message.member.id; 
         console.log(players.length);
 
+        // delete stat command after saying it!
+        message.delete();
+
         // checks through our players of players to see what to do next
         for (var i = 0; i < players.length; i++)
         {
             // this places a new player into our players
             if (play_id == players[i].play_id)
-            {
-                message.reply("Health: " + players[i].health);
-                message.reply("Level: " + players[i].level);
-                message.reply("Strength: " + players[i].strength);
-                message.reply("Defense: " + players[i].defense);                
+            { 
+                // how to DM users anything - these two commands right here
+                let user = players[i].player;       
+                return user.send("Health: " + players[i].health + "\n" + "Level: " + players[i].level + "\n" + "Strength: " + players[i].strength + "\n" + "Defense: " + players[i].defense);              
             }
         
             // warning if your health is low
             if (players[i].health > 0 && players[i].health < 11) {
-                message.reply("You're actually about to die, my dude.");
+                message.reply("A fellow player is on death's door.");
             }
             // resets health if you "die"
             if (players[i].health <= 0) {
                 players[i].health = 100;
-                message.reply("Dead. Resetting health. Now back at 100.");
+                message.reply("A player has died!");
             }
         }
             
