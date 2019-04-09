dfe5b2821d3b07e26e510efb51797f6c53405d95
JohnnySn0w <michael.mahan43@gmail.com>
2019-04-09 16:28:59 -0500

    ehhhh

--- DiscordBot/commands/mud/move.js
+++ DiscordBot/commands/mud/move.js
@@ -73,7 +73,7 @@ class MoveCommand extends commando.Command {
   movePlayer(message, player, direction, room) {
     if (direction in room.exits) {
       // if a room exists in the given direction, use that direction's associated room ID to get the next room
-      db.getItem(room.exits[direction], 'rooms', (data) => this.getRoom(message, data, direction, false));
+      db.getItem(room.exits[direction], 'rooms', (data) => this.getRoom(message, player, data, direction, false));
     }
     else {
       // otherwise, alert the player of the lack of exits
