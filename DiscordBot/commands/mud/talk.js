a333045209c3a6191b09ac1d77062ebb4aa84a63
eortiz <eric.ortiz@pioneerrx.com>
2019-04-13 21:51:03 -0500

    Still refactoring the talk command

--- DiscordBot/commands/mud/talk.js
+++ DiscordBot/commands/mud/talk.js
@@ -53,14 +53,12 @@ class TalkCommand extends commando.Command {
       // determine if the player is talking to an NPC
       if (room.npcs[entity]) {
         db.getItem(room.npcs[entity], 'npcs', (data) => this.getProgress(message, player, room, data));
-      }
-      else {
+      } else {
         // if not an npc, determine if the player is talking to an enemy
         if (room.enemies[entity]) {
           // if an enemy, then mention that the player can't talk to the enemy
           message.channel.send(`The ${entity} would rather a fight than a chat.`);
-        }
-        else {
+        } else {
           // if not an enemy either, they're talking to no one
           message.channel.send(`${player.name} is conversing with no one.`);
         }
@@ -79,8 +77,7 @@ class TalkCommand extends commando.Command {
 
     if (npc === undefined) {
       message.channel.send(`${player.name} is conversing with unseen forces.`);
-    }
-    else {
+    } else {
       const [response, responseNum] = this.determineResponse(npc, player);
       this.replyToPlayer(player, message, npc, response, responseNum, room, false);
     }
@@ -91,91 +88,90 @@ class TalkCommand extends commando.Command {
     let response;
     let responseNum = 0;
 
-    if (npc !== undefined) {
-      // find player's progress with this npc
-      if (player.progress.npc[npc.id]) { // check if talked to this npc before
-        let progress = player.progress.npc[npc.id];
-        if (npc.responses[progress]) {
-          response = npc.responses[progress].reply;
-		      if (progress == "0" || !(npc.goods.length > 0)) {
-			      for (var i = 0; i < npc.responses[progress].prompts.length; i++) {
-			        response = response + '\n' + npc.responses[progress].prompts[i].prompt;
-			        responseNum = responseNum + 1;
-			      }
-		      } else {
-			      response = response + `\n[[You have ${player.gold} gold.]]`;
-			      for (var i = 0; i < npc.goods.length; i++) {
-              if(!npc.goods[i].soldOut) { //if its not sold out, list it! :^)
-			          response = response + '\n [' + i + '] ' +  npc.goods[i].item + ' - ' + npc.goods[i].cost + ' gold';
-              }
-			        responseNum = responseNum + 1;
-			      }
-		      }
-        }
-      } else { 
-        // haven't talked to this npc before? 
-        //NBD create npc progress in the player object and get chattin :)
-        player.progress.npc[npc.id] = '0';
-        response = npc.responses['0'].reply;
-        for (let i = 0; i < npc.responses['0'].prompts.length; i++) {
-          response = `${response}\n${npc.responses['0'].prompts[i].prompt}`;
-			    responseNum = responseNum + 1;
+    // find player's progress with this npc
+    if (player.progress.npc[npc.id]) {
+      // check if talked to this npc before
+      let progress = player.progress.npc[npc.id];
+
+      if (npc.responses[progress]) {
+        response = npc.responses[progress].reply;
+        if (progress == "0" || !(npc.goods.length > 0)) {
+          for (var i = 0; i < npc.responses[progress].prompts.length; i++) {
+            response = response + '\n' + npc.responses[progress].prompts[i].prompt;
+            responseNum = responseNum + 1;
+          }
+        } else {
+          response = response + `\n[[You have ${player.gold} gold.]]`;
+          for (var i = 0; i < npc.goods.length; i++) {
+            if(!npc.goods[i].soldOut) { //if its not sold out, list it! :^)
+              response = response + '\n [' + i + '] ' +  npc.goods[i].item + ' - ' + npc.goods[i].cost + ' gold';
+            }
+            responseNum = responseNum + 1;
+          }
         }
       }
-    }        
+    } else { 
+      // haven't talked to this npc before? 
+      //NBD create npc progress in the player object and get chattin :)
+      player.progress.npc[npc.id] = '0';
+      response = npc.responses['0'].reply;
+
+      for (let i = 0; i < npc.responses['0'].prompts.length; i++) {
+        response = `${response}\n${npc.responses['0'].prompts[i].prompt}`;
+        responseNum = responseNum + 1;
+      }
+    }
+    
+    // return the npc's response and the number of replies the player can give to the npc
     return [response, responseNum];
   }
 
   // respond to the npc's response
-  replyToPlayer(player, message, person, response, responseNum, room, stop) {
+  replyToPlayer(player, message, npc, response, responseNum, room, stop) {
     let responded = false;
-    let progress = player.progress.npc[person.id];
-    if (!(person.responses === undefined)) {
-      if (!(person.hostile)) {
-        message.reply(`${person.name} says: ${response}`);
+    let progress = player.progress.npc[npc.id];
+    if (npc.responses !== undefined) {
+      message.reply(`${npc.name} says: ${response}`);
 
-        if (!stop) {
-          // responses change to using length
-          const filter = m => (((m.content < responseNum) && (Number.isInteger(Number(m.content)))) || (m.content.includes('?talk'))) && m.author.id === message.author.id; //only accepts responses in key and only from the person who started convo
-          const collector = message.channel.createMessageCollector(filter, {time: 20000});
-  
-          collector.on('collect', m => {
-            responded = true;
-            // stops collector
-            if ((person.goods.length > 0) && progress != '0')
-            {
-              if (person.goods[m.content].soldOut) {
-                return;
-              } else {
-                collector.stop();
-              }
-            } else { collector.stop(); }            
-            if (m.content.includes('?talk')) {
-              let newResponse = 'Oh ok bye';
-              this.replyToPlayer(player, message, person, newResponse, 1, room, true);
-            } else {    
-              this.makeProgress(player, person, m.content, message);
-              let [newResponse, newResponseNum] = this.determineResponse(person, player);
-              this.replyToPlayer(player, message, person, newResponse, newResponseNum, room, false);
+      if (!stop) {
+        // responses change to using length
+        const filter = m => (((m.content < responseNum) && (Number.isInteger(Number(m.content)))) || (m.content.includes('?talk'))) && m.author.id === message.author.id; //only accepts responses in key and only from the person who started convo
+        const collector = message.channel.createMessageCollector(filter, {time: 20000});
+
+        collector.on('collect', m => {
+          responded = true;
+          // stops collector
+          if ((npc.goods.length > 0) && progress != '0')
+          {
+            if (npc.goods[m.content].soldOut) {
+              return;
+            } else {
+              collector.stop();
             }
-            
-          });
-  
-          collector.on('end', () => {
-            if (!responded) {
-              message.channel.send(`${person.name} walked away from ${player.name}`);
-              if (person.goods.length > 0){ // let shopkeeps reset back to their first state everytime...
-                player.progress.npc[person.id] = '0';
-                db.updateItem(player.id, ['progress'], [player.progress], 'players', () => {});
-              }
+          } else { collector.stop(); }            
+          if (m.content.includes('?talk')) {
+            let newResponse = 'Oh ok bye';
+            this.replyToPlayer(player, message, npc, newResponse, 1, room, true);
+          } else {    
+            this.makeProgress(player, npc, m.content, message);
+            let [newResponse, newResponseNum] = this.determineResponse(npc, player);
+            this.replyToPlayer(player, message, npc, newResponse, newResponseNum, room, false);
+          }
+          
+        });
+
+        collector.on('end', () => {
+          if (!responded) {
+            message.channel.send(`${npc.name} walked away from ${player.name}`);
+            if (npc.goods.length > 0){ // let shopkeeps reset back to their first state everytime...
+              player.progress.npc[npc.id] = '0';
+              db.updateItem(player.id, ['progress'], [player.progress], 'players', () => {});
             }
-          });
-        }
-      } else {
-        message.channel.send(`${person.name} would rather a fight than a chat.`);
+          }
+        });
       }
     } else {
-      message.channel.send(`${person.name} is quite quiet.`);
+      message.channel.send(`${npc.name} has nothing to say.`);
     }
   }
 
