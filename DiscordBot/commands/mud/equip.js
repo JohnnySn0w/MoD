1923c467d381a3eb14bced00642e2a3c09c91a3a
DJSanti <samantha.e.santiago@gmail.com>
2019-04-29 18:31:27 -0500

    Started officially working on equip.

--- DiscordBot/commands/mud/equip.js
+++ DiscordBot/commands/mud/equip.js
@@ -12,27 +12,39 @@ class EquipCommand extends commando.Command {
     });
   }
 
-  async run(message) {
-    db.getItem(message.member.id, 'players', (data) => this.getPlayer(data, message));
+  async run(message, args) {
+    db.getItem(message.member.id, 'players', (data) => this.getPlayer(data, message, args));
     // delete the user's command if not debugging
     if (!DEBUG) {
       message.delete();
     }
   }
 
-  getPlayer(data, message) {
+  getPlayer(data, message, args) {
     // grab the actual player object
     var body = JSON.parse(data.body);
     var player = body.Item;
+    args = args;
 
         if (player === undefined) {
             // if the player isn't in the database already, send them a notice that they need to "?start" the game
             message.member.send("You need to start your adventure first! Please go to the testing zone and enter the start command to proceed.");
         }
         else {
-            // otherwise, do stuff here        
-        }
+            // do something
+            console.log(args);
     }
+  }
+
+  determineItem(searchName, room) {
+    // determine what item the player is looking at in the given room
+    var itemObject;
+    if (searchName in room.items) {
+      itemObject = room.items[searchName];
+    }
+
+    return itemObject;
+  }
   }
         
 
