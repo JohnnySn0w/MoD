3ca065d8edd15033b529a3aff5c19c0a0aaf4bf1
Eric Ortiz <e.ortiz.programmer@gmail.com>
2019-04-29 22:42:37 -0500

    Minor refactoring for attack damage rolling

--- DiscordBot/commands/mud/attack.js
+++ DiscordBot/commands/mud/attack.js
@@ -89,14 +89,14 @@ class AttackCommand extends commando.Command {
     return args;
   }
 
-  calculateDamage(attacker, victim, weaponMod = 0) {
-      // calculate player damage on enemy and update value using weapon
-    let roll = Math.floor(Math.random() * 7);
+  calculateDamage(attacker, victim, weaponMod = 0, armorMod = 0) {
+    // calculate player damage on enemy and update value using weapon
+    let roll = Math.floor(Math.random() * attacker.strength / 2);
     //roll is now for base damage
-    let calcDamage = roll + attacker.strength + weaponMod; 
-    //calculate damage using enemy defense
-    calcDamage -= victim.defense;
-    return calcDamage;
+    let damage = roll + attacker.strength + weaponMod; 
+    // subtract the victim's defense and armor from the total damage
+    damage = damage - victim.defense - armorMod;
+    return damage;
   }
 
   combatLoop(message, player, enemy, room) {
@@ -104,31 +104,40 @@ class AttackCommand extends commando.Command {
     // experience counter
     let xp = 0;
     while (player.health > 0 && enemy.health > 0) {
-      let damage = this.calculateDamage(player, enemy);
-      if(player.inventory.weapon != null){
+      let damage = 0;
+      if (player.inventory.weapon == null) {
+        damage = this.calculateDamage(player, enemy);
+      } else {
         damage = this.calculateDamage(player, enemy, player.weapon.stats);
-      } 
-          //make sure player isn't doing negative damage
-      if(damage > 0){
+      }
+
+      // if the player does "negative damage", they missed
+      if (damage > 0) {
         enemy.health = enemy.health - damage;
-        message.channel.send(`${player.name} hit ${enemy.name} for ${damage.toString()} damage.`);
-      } else{
+        message.channel.send(`${player.name} hit ${enemy.name} for ${damage} damage.`);
+        // increment experience counter based on the damage dealt
+        xp = xp + damage;
+      } else {
         message.channel.send(`${player.name} swung at the ${enemy.name} and missed.`);
       }
-        // increment experience counter; can vary depending on the enemy later
-      xp = xp + 5;
 
-      //prevents enemy attacking if dead
-      if(enemy.health > 0) {
-        //calculate enemy damage on agro target and update value
-          damage = this.calculateDamage(enemy, player); //for the following lines replace player with agro target
-          if(damage > 0){
-            player.health = player.health - damage;
-            message.channel.send(`${player.name} was hit by the ${enemy.name} for ${damage} damage.`);
-          } else{
-            message.channel.send(`${enemy.name} swung at the ${player.name} and missed.`);
-          }
-        }else {
+      // prevents enemy attacking if dead
+      if (enemy.health > 0) {
+        // calculate enemy damage on agro target and update value
+        let damage = 0;
+        if (player.inventory.armor == null) {
+          damage = this.calculateDamage(player, enemy);
+        } else {
+          damage = this.calculateDamage(enemy, player, 0, player.armor.stats);
+        }
+
+        if (damage > 0) {
+          player.health = player.health - damage;
+          message.channel.send(`${player.name} was hit by the ${enemy.name} for ${damage} damage.`);
+        } else {
+          message.channel.send(`${enemy.name} swung at the ${player.name} and missed.`);
+        }
+      } else {
         message.channel.send(`${player.name} defeated the ${enemy.name}.`);
       }
     }
