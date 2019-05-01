8021609f9549af4a4d482945ad92618201baf306
Eric Ortiz <e.ortiz.programmer@gmail.com>
2019-05-01 01:37:38 -0500

    Added, tested, and commented the discard command

--- DiscordBot/commands/mud/discard.js
+++ DiscordBot/commands/mud/discard.js
@@ -12,33 +12,80 @@ class DiscardCommand extends commando.Command {
     });
   }
 
-  async run(message) {
-    db.getItem(message.member.id, 'players', (data) => this.getPlayer(data, message));
+  async run(message, args) {
+    db.getItem(message.member.id, 'players', (data) => this.getPlayer(data, message, args));
     deleteMessage(message);
   }
 
-  getPlayer(data, message) {
+  getPlayer(data, message, args) {
     // grab the actual player object
     var body = JSON.parse(data.body);
     var player = body.Item;
 
+    // make sure the player is a part of the MUD
     if (player === undefined) {
-        // if the player isn't in the database already, send them a notice that they need to "?start" the game
-        message.member.send("You need to start your adventure first! Please go to the testing zone and enter the start command to proceed.");
-    }
-    else {
-        // otherwise, do stuff here        
+      message.member.send('It seems that you\'re not a part of the MUD yet! \nUse `?start` in test-zone to get started!');
+    } else {
+      // grab the item from keys first
+      var itemName = args.toLowerCase();
+      var item = this.getKey(player, itemName);
+
+      // check to make sure the item isn't a key
+      if (item === undefined) {
+        item = this.getItem(player, itemName);
+
+        // ensure the item exists
+        if (item === undefined) {
+          message.member.send(`It doesn't seem that you have the ${args} in your inventory.`);
+        } else {
+          // make sure the item isn't already equipped
+          if (item.equipped && item.amount == 1) {
+            message.member.send(`The ${item.name} is equipped, and it's your last item of its kind. Equip something else to discard it.`);
+          } else {
+            // discard the item
+            this.discardItem(message, player, item);
+          }
+        }
+      } else {
+        message.member.send(`${item.name} is a key item. You can't discard it.`);
+      }
     }
   }
+
+  getKey(player, itemName) {
+    // iterate through the player's list of items and check each one's name
+    for (var key in player.inventory.keys) {
+      if (itemName === player.inventory.keys[key].name.toLowerCase()) {
+        return player.inventory.keys[key];
+      }
+    }
+
+    return undefined;
+  }
+
+  getItem(player, itemName) {
+    // iterate through the player's list of items and check each one's name
+    for (var item in player.inventory.items) {
+      if (itemName === player.inventory.items[item].name.toLowerCase()) {
+        return player.inventory.items[item];
+      }
+    }
+
+    return undefined;
+  }
+
+  discardItem(message, player, item) {
+    // if the player has multiples of the item, just remove one - otherwise delete the entire item
+    if (item.amount > 1) {
+      player.inventory.items[item.id].amount -= 1;
+    } else {
+      delete player.inventory.items[item.id];
+    }
+
+    // update the player object and message the player
+    db.updateItem(player.id, ['inventory', 'equipment'], [player.inventory, player.equipment], 'players', () => {});
+    message.member.send(`${item.name} was discarded.`);
+  }
 }
-  /* plan:
-       - parse the args given so that we can get the given words into a single string (ex. 'Short' 'Sword' becomes 'Short Sword')
-       - compare this string to what's in player inventory to see if we have the item 
-       - if we have it, we then see if it's been assigned to a weapon or armor slot.
-          - if it has, we update player attributes by subtracting the strength or defense value of the item from the player's
-          - the slot is then made null
-       - ultimately, we delete the item from player inventory
-       - of course, if we don't have the item, we don't do any of this
-  */
 
 module.exports = DiscardCommand;
