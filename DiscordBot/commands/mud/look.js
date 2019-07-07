8291b0087a516f0758c145fd755fce68e1f12d91
JohnnySn0w <michael.mahan43@gmail.com>
2019-07-07 12:24:57 -0500

    remove broken commit changes

--- DiscordBot/commands/mud/look.js
+++ DiscordBot/commands/mud/look.js
@@ -16,13 +16,13 @@ class LookCommand extends commando.Command {
     super(client, COMMAND_CONSTANT(
       'look',
       LookCommand.commandInfo(),
-      false,
+      true,
       LookCommand.aliases(),
     ));
   }
 
   async run(message, args) {
-    bigCheck(message, this.getRoom.bind(this), args.object ? args.object : '');
+    bigCheck(message, this.getRoom.bind(this), args.object);
     deleteMessage(message);
   }
 
