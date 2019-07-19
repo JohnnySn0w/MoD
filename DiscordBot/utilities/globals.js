369ce2eca5906c6ddca8b45f16a29cb6c0bf623b
JohnnySn0w <michael.mahan43@gmail.com>
2019-07-18 21:55:47 -0500

    update db in globals

--- DiscordBot/utilities/globals.js
+++ DiscordBot/utilities/globals.js
@@ -1,4 +1,4 @@
-const db = require('./dbhandler');
+const { getItem, updateItem } = require('./dbhandler');
 const { ITEM_CONSTANT } = require('../Constants/itemConstant');
 
 const DEBUG = true;
@@ -29,25 +29,23 @@ function bigCheck(message, callback,  args = '') {
   if(args !== '') {
     args = args.toLowerCase();
   }
-  db.getItem(message.author.id, 'players', (data) => playerCheck(data, callback, message, args));
+  getItem(message.author.id, 'players', (data) => playerCheck(data, callback, message, args));
 }
 
 function playerCheck(data, callback, message, args) {
   let player = JSON.parse(data.body).Item;
-  
   if (player === undefined) {
     message.member.send('It seems that you\'re not a part of the MUD yet! \nUse `${commandPrefix}start` in #start-here to get started!');
   } else if (player.busy) {
     message.channel.send(`${player.characterName} is trying to multitask, and failing.`);
   } else {
-    db.getItem(message.channel.name, 'rooms', (moreData) => roomCheck(player, message, moreData, callback, args));
+    getItem(player.currentRoomId, 'rooms', (moreData) => roomCheck(player, message, moreData, callback, args));
   }
 }
 
 function roomCheck(player, message, data, callback, args) {
   // grab the actual player object
   const room = JSON.parse(data.body).Item;
-
   if (!dmOnly) {
     if (room === undefined) {
       message.author.send('You are not in a MUD related room.');
@@ -59,7 +57,7 @@ function roomCheck(player, message, data, callback, args) {
 
 function respawn(message, player) {
   // respawn player
-  db.updateItem(player.id, ['health', 'busy', 'currentRoomId'], [player.maxhealth, false, player.hearth],'players', () => {});
+  updateItem(player.id, ['health', 'busy', 'currentRoomId'], [player.maxhealth, false, player.hearth],'players', () => {});
   message.member.setRoles([message.guild.roles.find(role => role.name === 'village-square')]).catch(console.error);
 }
 
@@ -90,7 +88,7 @@ function discardItem(player, item) {
     delete player.inventory.items[item.id];
   }
   // update the player object and message the player
-  db.updateItem(player.id, ['inventory', 'equipment'], [player.inventory, player.equipment], 'players', () => {});
+  updateItem(player.id, ['inventory', 'equipment'], [player.inventory, player.equipment], 'players', () => {});
 }
 
 function inventoryAddItem(itemData, player, callback) {
@@ -115,7 +113,7 @@ function inventoryAddItem(itemData, player, callback) {
       player.inventory.items[item.id] = ITEM_CONSTANT(item);
     }
   }
-  db.updateItem(
+  updateItem(
     player.id,
     ['inventory'],
     [player.inventory],
@@ -164,7 +162,7 @@ function onlineCheck({ body }, member, content) {
 //server level messaging
 function sendMessageGlobal(message, content) {
   message.guild.fetchMembers().forEach(member => {
-    db.getItem(member.id, 'players', (data) => onlineCheck(data, member, content) );
+    getItem(member.id, 'players', (data) => onlineCheck(data, member, content) );
   });
   if (!dmOnly) {
     message.guild.channels.forEach(channel => {
