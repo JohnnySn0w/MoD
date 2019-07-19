0821c07fe144e1c1ebd2daef356d04c9b9580b82
JohnnySn0w <michael.mahan43@gmail.com>
2019-07-18 21:29:51 -0500

    callbacks no longer necessary for db

--- DiscordBot/utilities/dbhandler.js
+++ DiscordBot/utilities/dbhandler.js
@@ -9,26 +9,26 @@ function createResponse(statusCode, message) {
   };
 }
 
-module.exports.saveItem = async (item, table, callback) => {
+module.exports.saveItem = async (item, table, callback=()=>{}) => {
 //   console.log('handler received: \n', item);
   databaseManager.saveItem(item, table).then(response => {
     callback(createResponse(200, response));
   });
 };
 
-module.exports.getItem = async (itemId, table, callback) => {
+module.exports.getItem = async (itemId, table, callback=()=>{}) => {
   const response = await databaseManager.getItem(itemId, table);
   callback(createResponse(200, response));
 };
 
 //this shouldn't need to be used, if you're updating an entry, use updateItem
-module.exports.deleteItem = async (itemId, table, callback) => {
+module.exports.deleteItem = async (itemId, table, callback=()=>{}) => {
   databaseManager.deleteItem(itemId, table).then(response => {
     callback(createResponse(200, 'Item was deleted', response));
   });
 };
 
-module.exports.updateItem = async (itemId, paramName, paramValue, table, callback) => {
+module.exports.updateItem = async (itemId, paramName, paramValue, table, callback=()=>{}) => {
   databaseManager.updateItem(itemId, paramName, paramValue, table).then(response => {
     // console.log('handler updated: \n', item);
     callback(createResponse(200, response));
