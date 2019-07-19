a1d6d32d06b7feba26e3c4f07d740a7912dad1ba
JohnnySn0w <michael.mahan43@gmail.com>
2019-07-18 21:29:33 -0500

    move now changes player's internal location

--- DiscordBot/commands/mud/move.js
+++ DiscordBot/commands/mud/move.js
@@ -1,6 +1,6 @@
 const {deleteMessage, bigCheck, commandPrefix} = require('../../utilities/globals');
 const commando = require('discord.js-commando');
-const db = require('../../utilities/dbhandler');
+const { getItem, updateItem} = require('../../utilities/dbhandler');
 const { COMMAND_CONSTANT } = require('../../Constants/commandConstant');
 
 class MoveCommand extends commando.Command {
@@ -39,9 +39,10 @@ class MoveCommand extends commando.Command {
         const actualRoom = JSON.parse(room.body).Item;
         const nextRoom = this.client.channels.find(channel => channel.name === actualRoom.id);
         // if we're grabbing the room that the player is moving to, assign the player the new room's role ID
-        message.channel.send(`${player.characterName} moved to <#${nextRoom.id}>`);
+        message.channel.send(`${player.characterName} has left`);
         const roomRole = message.guild.roles.find(role => role.name === actualRoom.id);
         message.member.setRoles([roomRole]).catch(e => console.error(e));
+        updateItem(player.id, ['currentRoomID'], [actualRoom.id], 'rooms');
         nextRoom.send(`${player.characterName} has entered.`);
       }
     } else {
@@ -52,7 +53,7 @@ class MoveCommand extends commando.Command {
   movePlayer(message, player, direction, room) {
     if (direction in room.exits) {
       // if a room exists in the given direction, use that direction's associated room ID to get the next room
-      db.getItem(room.exits[direction], 'rooms', (data) => this.getRoom(message, player, data, direction, false));
+      getItem(room.exits[direction], 'rooms', (data) => this.getRoom(message, player, data, direction, false));
     }
     else {
       // otherwise, alert the player of the lack of exits
